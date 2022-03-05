3c61d9073f626513063db4a1ab1843a30546b7b6
sargis <abovyan.sargis90@gmail.com>
2022-03-05 11:05:53 +0400

    useRef setup for carousel

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,19 @@ import { useState, createRef, useRef, useEffect, useMemo } from "react";
 
 let outerRef = null;
 
+const listStyles = { listStyle: "none", display: "flex", gap: "1rem" };
+
+const listItemStyles = {
+  width: 200,
+  maxWidth: 200,
+  border: "1px solid red",
+  aspectRatio: "1/1",
+  borderRadius: "5px",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+};
+
 function App() {
   const inputRef = useRef(null);
   const anotherRef = useRef(null);
@@ -19,18 +32,28 @@ function App() {
   }, [state]);
 
   return (
-    <div className="App">
-      <input
-        ref={inputRef}
-        type="text"
-        onFocus={() => {
-          console.log("I am focused");
-          setState(!state);
-
-          anotherRef.current = !anotherRef.current ? 1 : anotherRef.current + 1;
-        }}
-      />
-      <button>click</button>
+    <div className="App" style={{ padding: "1rem" }}>
+      <ul style={listStyles}>
+        {["one", "two", "three"].map((el) => (
+          <li style={listItemStyles} key={el}>
+            <span>{el}</span>
+          </li>
+        ))}
+      </ul>
+      <div style={{ width: 200 }}>
+        <div
+          className="buttonWrapper"
+          style={{
+            marginTop: "1rem",
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <button>Prev</button>
+          <button>Next</button>
+        </div>
+      </div>
     </div>
   );
 }
